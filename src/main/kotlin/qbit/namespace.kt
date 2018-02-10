a83d2a3586bee33ec72405cf55623ef59e21e275
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2018-02-10 21:11:22 +0700

    Implement view of namespace as list of parts

--- src/main/kotlin/qbit/namespace.kt
+++ src/main/kotlin/qbit/namespace.kt
@@ -1,6 +1,12 @@
 package qbit
 
+import java.util.Arrays.asList
+
 data class Namespace(val name: String, val parent: Namespace? = null) {
+
+    val parts: List<String> =
+            if (parent == null) asList(name)
+            else parent.parts + name
 
     operator fun get(key: String) = Key(this, key)
 
