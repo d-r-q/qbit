f5f13422ac5fae443a1c6dca1e43b5e1fc519605
Aleksey Zhidkov <aleksey.zhidkov@pm.me>
2019-04-17 12:57:11 +0700

    Implement simple entity to typed interface mapping

---
+++ src/main/kotlin/qbit/mapping/EntityProxy.kt
@@ -0,0 +1,63 @@
+package qbit.mapping
+
+import qbit.Db
+import qbit.EID
+import qbit.Entity
+import qbit.IdentifiedEntity
+import java.lang.IllegalStateException
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
+
+interface EntityHolder {
+
+    fun entity(): Entity
+
+    fun eid(): EID?
+
+}
+
+private class EntityProxy<T>(private val entity: Entity, private val clazz: Class<T>) : InvocationHandler {
+
+    private val method2attr = entity.keys
+            .map { it.name.name to it }
+            .toMap()
+
+    override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any? {
+        return if (method.name == "entity" && method.returnType == Entity::class.java) {
+            return entity
+        } else if (method.name == "eid" && method.returnType == EID::class.java) {
+            return (entity as? IdentifiedEntity)?.eid
+        } else if (entity.javaClass.methods.contains(method)) {
+            if (args == null || args.isEmpty()) {
+                method.invoke(entity)
+            } else {
+                method.invoke(entity, args)
+            }
+        } else {
+            val name = method.name
+            if (args == null && name in method2attr) {
+                entity[method2attr.getValue(name)]
+            } else if (args?.size == 1 && name in method2attr) {
+                proxy(entity.set(method2attr.getValue(name), args[0]), clazz)
+            } else {
+                throw IllegalStateException("Method $method could not be implemented")
+            }
+        }
+    }
+
+}
+
+inline fun <reified T> proxy(entity: Entity): T {
+    return proxy(entity, T::class.java)
+}
+
+fun <T> proxy(entity: Entity, clazz: Class<T>): T {
+    @Suppress("UNCHECKED_CAST")
+    return Proxy.newProxyInstance(
+            EntityProxy::class.java.classLoader,
+            arrayOf<Class<*>>(clazz),
+            EntityProxy(entity, clazz)) as T
+}
+
+inline fun <reified T> Db.pullAs(eid: EID): T? = this.pull(eid)?.let { proxy(it, T::class.java) }
--- src/test/kotlin/qbit/demo.kt
+++ src/test/kotlin/qbit/demo.kt
@@ -1,5 +1,8 @@
 package qbit
 
+import qbit.mapping.EntityHolder
+import qbit.mapping.proxy
+import qbit.mapping.pullAs
 import qbit.ns.Namespace
 import qbit.schema.*
 import qbit.storage.MemStorage
@@ -8,6 +11,15 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 val HHmm = DateTimeFormatter.ofPattern("HH:mm")
+
+interface User : Entity, EntityHolder {
+
+    fun name(): String
+    fun name(newName: String): User
+
+    fun last_login(): Instant
+
+}
 
 fun main() {
     val tweetNs = Namespace.of("demo", "tweet")
@@ -28,7 +40,7 @@ fun main() {
     val tweet = Entity(content eq "Hello @HackDay",
             author eq user,
             date eq ZonedDateTime.now())
-    val storedUser = conn.persist(tweet).createdEntities[user]!!
+    val storedUser = conn.persist(tweet).createdEntities.getValue(user)
     conn.persist(storedUser.set(lastLogin, Instant.now()))
 
     val sTweet = conn.db.query(attrIs(content, "Hello @HackDay")).first()
@@ -40,4 +52,11 @@ fun main() {
 
     println(nTweet[content])
     println(nTweet[likes].map { it[name] })
+    val users = nTweet[likes].map { proxy<User>(it) }
+    println(users.map { p -> "${p.name()}: ${p.last_login()}" })
+
+    val nUser = users[0].name("@reflection_rulezz")
+    conn.persist(nUser.entity())
+    println(conn.db.pullAs<User>(nUser.eid()!!)!!.name())
+
 }
