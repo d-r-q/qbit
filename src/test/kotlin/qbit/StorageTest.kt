0e9017130efb71485664f6d6a3d1326452fc9040
S1x6 <v.korshunov@g.nsu.ru>
2019-07-01 18:37:57 +0700

    - Killed mutation:      at conn:105: storage.overwrite(Namespace("refs")["head"], newHead.hash.bytes)      at FileSystemStorage:46: writeAndSync(file, value)      at FileSystemStorage:53: return null      ignored at FileSystemStorage:68: return dir.listFiles { f -> f.isDirectory }

--- src/test/kotlin/qbit/StorageTest.kt
+++ src/test/kotlin/qbit/StorageTest.kt
@@ -2,8 +2,12 @@ package qbit
 
 import org.junit.Assert
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Test
+import qbit.model.*
 import qbit.ns.Namespace
+import qbit.ns.root
 import qbit.storage.FileSystemStorage
 import qbit.storage.MemStorage
 import qbit.storage.Storage
@@ -37,6 +41,45 @@ class StorageTest {
 
         Assert.assertArrayEquals(subBytes, storage.load(subNs["sub-data"]))
         assertEquals(setOf(subNs["sub-data"]), storage.keys(subNs).toSet())
+    }
+
+    @Test
+    fun testSwapHead() {
+        val user = Namespace("user")
+        val _id = ScalarAttr(user["val"], QString)
+
+        val root = Files.createTempDirectory("qbit").toFile()
+        val storage = FileSystemStorage(root)
+
+        val conn = qbit(storage)
+        conn.persist(_id)
+
+        val e = Entity(_id eq "1")
+        val trx = conn.trx()
+        trx.persist(e)
+        trx.commit()
+
+        val loaded = storage.load(Namespace("refs")["head"])
+        val hash = conn.db.hash.bytes
+        Assert.assertArrayEquals(loaded, hash)
+    }
+
+    @Ignore
+    fun testCopyNsConstructor() {
+        val user = Namespace("nodes").subNs("test")
+        val _id = ScalarAttr(user["val"], QString)
+
+        val mem = MemStorage()
+        val conn = qbit(mem)
+        conn.persist(_id)
+
+        val e = Entity(_id eq "1")
+        conn.persist(e)
+
+        val rootFile = Files.createTempDirectory("qbit").toFile()
+        val storage = FileSystemStorage(rootFile)
+        assertTrue(mem.subNamespaces(user.parent!!) == storage.subNamespaces(user.parent!!))
+
     }
 
 }
