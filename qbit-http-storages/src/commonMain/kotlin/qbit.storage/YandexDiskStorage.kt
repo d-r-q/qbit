7e870c849c2005d7d2da79159ec1ce656f4df8ee
Fedor Pushkov <f.pushkov@g.nsu.ru>
2019-10-30 23:51:40 +0700

    some cleanup

--- qbit-http-storages/src/commonMain/kotlin/qbit.storage/YandexDiskStorage.kt
+++ qbit-http-storages/src/commonMain/kotlin/qbit.storage/YandexDiskStorage.kt
@@ -8,6 +8,7 @@ import io.ktor.client.request.parameter
 import io.ktor.client.request.put
 import io.ktor.client.response.HttpResponse
 import io.ktor.client.response.readBytes
+import io.ktor.client.response.readText
 import io.ktor.content.ByteArrayContent
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -32,10 +33,10 @@ class YandexDiskStorage(private val accessToken: String) : Storage {
         val paths = generatePutResourcePaths(key.ns)
         try {
             createFolderStructure(client, paths)
-        } catch(e: ClientRequestException){
+        } catch (e: ClientRequestException) {
             println(e)
         }
-        val json = Json.indented.parseJson(getUrlToUploadFile(client, paths[paths.size - 1], key.name))
+        val json = Json.indented.parseJson(getUrlToUploadFile(client, paths[paths.size - 1], key.name).readText())
         var urlToUploadFile = (json as JsonObject).getPrimitive("href").content
         var filePutResponse = uploadFile(client, urlToUploadFile, value)
         client.close()
@@ -45,7 +46,7 @@ class YandexDiskStorage(private val accessToken: String) : Storage {
     override fun overwrite(key: Key, value: ByteArray) = runBlocking {
         val client = HttpClient()
         val paths = generatePutResourcePaths(key.ns)
-        val json = Json.indented.parseJson(getUrlToUploadFile(client, paths[paths.size - 1], key.name))
+        val json = Json.indented.parseJson(getUrlToUploadFile(client, paths[paths.size - 1], key.name).readText())
         var urlToUploadFile = (json as JsonObject).getPrimitive("href").content
         var filePutResponse = uploadFile(client, urlToUploadFile, value)
         client.close()
@@ -57,7 +58,7 @@ class YandexDiskStorage(private val accessToken: String) : Storage {
 
         val client = HttpClient()
         val response = getUrlToDownload(client, filePath)
-        val json = Json.indented.parseJson(response)
+        val json = Json.indented.parseJson(response.readText())
         val downloadUrl = (json as JsonObject).getPrimitive("href").content
 
         val byteArray = getFile(client, downloadUrl)
@@ -70,12 +71,12 @@ class YandexDiskStorage(private val accessToken: String) : Storage {
         var fullPath = getFullPath(namespace)
 
         val client = HttpClient()
-        val response = client.get<String>(YANDEX_DISK_API_RESOURCES){
+        val response = client.get<HttpResponse>(YANDEX_DISK_API_RESOURCES) {
             parameter("path", fullPath)
             header("Authorization", "OAuth $accessToken")
         }
         val json = Json(JsonConfiguration.Stable)
-        val resource = json.parse(Resource.serializer(), response)
+        val resource = json.parse(Resource.serializer(), response.readText())
         val fileNames = getFileNamesInResourceByType(resource, "file")
         val keys = wrapFileNamesToKeys(fileNames, namespace)
         client.close()
@@ -87,12 +88,12 @@ class YandexDiskStorage(private val accessToken: String) : Storage {
         var fullPath = getFullPath(namespace)
 
         val client = HttpClient()
-        val response = client.get<String>(YANDEX_DISK_API_RESOURCES){
+        val response = client.get<HttpResponse>(YANDEX_DISK_API_RESOURCES) {
             parameter("path", fullPath)
             header("Authorization", "OAuth $accessToken")
         }
         val json = Json(JsonConfiguration.Stable)
-        val resource = json.parse(Resource.serializer(), response)
+        val resource = json.parse(Resource.serializer(), response.readText())
         val dirNames = getFileNamesInResourceByType(resource, "dir")
         val namespaces = wrapDirNamesToNamespaces(dirNames, namespace)
         client.close()
@@ -111,103 +112,100 @@ class YandexDiskStorage(private val accessToken: String) : Storage {
             }
             client.close()
             true
-        } catch(e: ClientRequestException){
+        } catch (e: ClientRequestException) {
             client.close()
             false
         }
     }
 
-    private suspend fun uploadFile(client: HttpClient, urlToUploadFile: String, byteArray: ByteArray) : String{
-        val response = client.put<String>(urlToUploadFile){
+    private suspend fun uploadFile(client: HttpClient, urlToUploadFile: String, byteArray: ByteArray): HttpResponse {
+        return client.put(urlToUploadFile) {
             body = ByteArrayContent(byteArray)
-        }
-        return response;
+        };
     }
 
-    private suspend fun getUrlToUploadFile(client: HttpClient, filePath: String, fileName: String) : String{
-        val response = client.get<String>(YANDEX_DISK_API_GET_FILE_URL) {
+    private suspend fun getUrlToUploadFile(client: HttpClient, filePath: String, fileName: String): HttpResponse {
+        return client.get(YANDEX_DISK_API_GET_FILE_URL) {
             parameter("path", filePath + fileName)
             parameter("overwrite", true)
-            header("Authorization", "OAuth $accessToken");
+            header("Authorization", "OAuth $accessToken")
         }
-        return response
     }
 
-    private suspend fun createFolderStructure(client: HttpClient, paths : List<String>){
-        val iterator = paths.iterator();
-        while(iterator.hasNext()){
-            val path = iterator.next();
+    private suspend fun createFolderStructure(client: HttpClient, paths: List<String>) {
+        val iterator = paths.iterator()
+        while (iterator.hasNext()) {
+            val path = iterator.next()
             client.put<String>(YANDEX_DISK_API_RESOURCES) {
                 parameter("path", path)
-                header("Authorization", "OAuth $accessToken");
+                header("Authorization", "OAuth $accessToken")
             }
         }
     }
 
-    //    Helper method for wrapping dirNames to Namespace Collection
-    private fun wrapDirNamesToNamespaces(dirNames: List<String>, namespace: Namespace) : Collection<Namespace> {
+    private suspend fun getFile(client: HttpClient, downloadUrl: String): ByteArray {
+        val httpResponse = client.get<HttpResponse>(downloadUrl)
+        return httpResponse.readBytes()
+    }
+
+    private suspend fun getUrlToDownload(client: HttpClient, path: String): HttpResponse {
+        return client.get(YANDEX_DISK_API_GET_DOWNLOAD_FILE_URL) {
+            parameter("path", path)
+            header("Authorization", "OAuth $accessToken")
+        };
+    }
+
+    /**    Helper method for wrapping dirNames to Namespace Collection */
+    private fun wrapDirNamesToNamespaces(dirNames: List<String>, namespace: Namespace): Collection<Namespace> {
         return dirNames.map { dirName -> Namespace(namespace, dirName) }
     }
 
-    //    Helper method for wrapping fileNames to Keys Collection
+    /**    Helper method for wrapping fileNames to Keys Collection */
     private fun wrapFileNamesToKeys(fileNames: List<String>, namespace: Namespace): Collection<Key> {
-        return fileNames.map{ filename -> Key(namespace, filename)}
+        return fileNames.map { filename -> Key(namespace, filename) }
     }
 
-    //    Helper method for getting file names by type
-    private fun getFileNamesInResourceByType(resource: Resource, type: String): List<String>{
+    /**    Helper method for getting file names by type */
+    private fun getFileNamesInResourceByType(resource: Resource, type: String): List<String> {
         val items = resource._embedded?.items
         var files: List<String> = emptyList()
-        if(items != null){
-            files = items.filter { item -> item.type == type}.map { item -> item.name }
+        if (items != null) {
+            files = items.filter { item -> item.type == type }.map { item -> item.name }
         }
         return files
     }
 
-    //    Helper method for getting full path from namespace to root
-    private fun getFullPath(namespace: Namespace) : String {
-        var parts = namespace.parts;
-        return parts.joinToString("/") + "/";
+    /**    Helper method for getting full path from namespace to root */
+    private fun getFullPath(namespace: Namespace): String {
+        var parts = namespace.parts
+        return parts.joinToString("/") + "/"
     }
 
-    //    Helper method for getting paths from namespace and its parents
-    private fun generatePutResourcePaths(namespace: Namespace) : List<String> {
+    /**    Helper method for getting paths from namespace and its parents */
+    private fun generatePutResourcePaths(namespace: Namespace): List<String> {
         val partsArrayList = arrayListOf<List<String>>()
         val pathsArrayList = arrayListOf<String>()
         var currentNamespace = namespace
-        while(currentNamespace.parent != null){
+        while (currentNamespace.parent != null) {
             partsArrayList.add(currentNamespace.parts)
             currentNamespace = currentNamespace.parent!!
         }
 
         val iterator = partsArrayList.iterator();
-        while(iterator.hasNext()) {
-            val parts = iterator.next();
-            pathsArrayList.add(parts.joinToString("/") + "/");
+        while (iterator.hasNext()) {
+            val parts = iterator.next()
+            pathsArrayList.add(parts.joinToString("/") + "/")
         }
-        return pathsArrayList.reversed();
+        return pathsArrayList.reversed()
     }
 
-    private suspend fun getFile(client: HttpClient, downloadUrl: String) : ByteArray{
-        val httpResponse = client.get<HttpResponse>(downloadUrl)
-        val byteArray = httpResponse.readBytes()
-        return byteArray;
-    }
-
-    private suspend fun getUrlToDownload(client: HttpClient, path: String): String {
-        val response = client.get<String>(YANDEX_DISK_API_GET_DOWNLOAD_FILE_URL) {
-            parameter("path", path)
-            header("Authorization", "OAuth $accessToken");
-        }
-        return response;
-    }
-
-    private fun getFullPath(key: Key) : String {
-        val fileName = key.name;
-        val namespace = key.ns;
-        val parts = namespace.parts;
+    /**    Helper method for getting full path to key, including key name */
+    private fun getFullPath(key: Key): String {
+        val fileName = key.name
+        val namespace = key.ns
+        val parts = namespace.parts
         val filePath = parts.joinToString("/")
-        return "$filePath/$fileName";
+        return "$filePath/$fileName"
     }
 }
 
@@ -226,20 +224,24 @@ fun <T> runBlocking(body: suspend () -> T): T {
 }
 
 @Serializable
-data class Resource(val antivirus_status: String? = null, val resource_id: String? = null, val share: ShareInfo? = null,
-                    val file: String? = null, val size: Int? = null, val photoslice_time: String? = null,
-                    val _embedded: ResourceList? = null, val exif: Exif? = null, val custom_properties: JsonObject? = null,
-                    val media_type: String? = null, val preview: String? = null, val type: String,
-                    val mime_type: String? = null, val revision: Long? = null, val public_url: String? = null, val path: String,
-                    val md5: String? = null, val public_key: String? = null, val sha256: String? = null, val name: String,
-                    val created: String, val modified: String, val comment_ids: CommentIds? = null)
+data class Resource(
+    val antivirus_status: String? = null, val resource_id: String? = null, val share: ShareInfo? = null,
+    val file: String? = null, val size: Int? = null, val photoslice_time: String? = null,
+    val _embedded: ResourceList? = null, val exif: Exif? = null, val custom_properties: JsonObject? = null,
+    val media_type: String? = null, val preview: String? = null, val type: String,
+    val mime_type: String? = null, val revision: Long? = null, val public_url: String? = null, val path: String,
+    val md5: String? = null, val public_key: String? = null, val sha256: String? = null, val name: String,
+    val created: String, val modified: String, val comment_ids: CommentIds? = null
+)
 
 @Serializable
 data class ShareInfo(val is_root: Boolean? = null, val is_owned: Boolean? = null, val rights: String)
 
 @Serializable
-data class ResourceList(val sort: String? = null, val items: Array<Resource>, val limit: Int? = null, val offset: Int? = null,
-                        val path: String, val total: Int? = null)
+data class ResourceList(
+    val sort: String? = null, val items: Array<Resource>, val limit: Int? = null, val offset: Int? = null,
+    val path: String, val total: Int? = null
+)
 
 @Serializable
 data class Exif(val date_time: String? = null)
