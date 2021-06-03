f972b44bf494d0ef2183c54cd2eb5d966a8c161a
Dmitriy Linevich <d.linevich@eastbanctech.ru>
2021-06-03 13:47:37 +0700

    Fix TrxGraphTest.kt

--- qbit-core/src/commonTest/kotlin/qbit/serialization/TrxGraphTest.kt
+++ qbit-core/src/commonTest/kotlin/qbit/serialization/TrxGraphTest.kt
@@ -1,10 +1,10 @@
 package qbit.serialization
 
+import io.ktor.utils.io.core.*
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
 import qbit.api.gid.Iid
 import qbit.api.model.Hash
-import qbit.api.model.nullHash
 import qbit.api.system.DbUuid
 import qbit.platform.runBlocking
 import kotlin.js.JsName
@@ -13,34 +13,42 @@ import kotlin.test.assertEquals
 
 class TrxGraphTest{
 
-    enum class BaseGraph {
-        NONE, AFTER, BEFORE
-    }
+    class GraphDescription(val root: Node<Hash>, val nodes: Map<String,Node<Hash>>)
 
-    private fun createNodesGraph(baseGraph: BaseGraph): Pair<Pair<NodeVal<Hash>?, NodeVal<Hash>>, List<NodeVal<Hash>>>{
+    private fun createNodesGraph(): GraphDescription {
         val testDbUuid = DbUuid(Iid(5, 2))
         val testTimestamp = 10L
         val testNodeData = NodeData(emptyArray())
-        val root = Root(nullHash, testDbUuid, testTimestamp, testNodeData)
-        val leaf1 = Leaf(nullHash, root, testDbUuid, testTimestamp, testNodeData)
-        val leaf2 = Leaf(nullHash, root, testDbUuid, testTimestamp, testNodeData)
-        val leaf3 = Leaf(nullHash, leaf1, testDbUuid, testTimestamp, testNodeData)
-        val merge1 = Merge(nullHash, root, leaf2, leaf3, testDbUuid, testTimestamp, testNodeData)
-        val leaf4 = Leaf(nullHash, merge1, testDbUuid, testTimestamp, testNodeData)
-        return when(baseGraph){
-            BaseGraph.AFTER -> (root to leaf4) to listOf(leaf2, leaf1, leaf3, merge1, leaf4)
-            BaseGraph.BEFORE -> (leaf1 to leaf4) to listOf(leaf3, merge1, leaf4)
-            BaseGraph.NONE -> (null to leaf4) to listOf(root, leaf2, leaf1, leaf3, merge1, leaf4)
-        }
+        val nodes = HashMap<String,Node<Hash>>()
+        val root = Root(Hash(nodes.size.toString().toByteArray()), testDbUuid, testTimestamp, testNodeData)
+        nodes["root"] = root
+        val leaf1 = Leaf(Hash(nodes.size.toString().toByteArray()), root, testDbUuid, testTimestamp, testNodeData)
+        nodes["leafBeforeRootAfterMergeParent"] = leaf1
+        val leaf2 = Leaf(Hash(nodes.size.toString().toByteArray()), root, testDbUuid, testTimestamp, testNodeData)
+        nodes["leafBeforeRootAndMergeParent"] = leaf2
+        val leaf3 = Leaf(Hash(nodes.size.toString().toByteArray()), leaf1, testDbUuid, testTimestamp, testNodeData)
+        nodes["mergeParent"] = leaf3
+        val merge = Merge(Hash(nodes.size.toString().toByteArray()), root, leaf2, leaf3, testDbUuid, testTimestamp, testNodeData)
+        nodes["merge"] = merge
+        val leaf4 = Leaf(Hash(nodes.size.toString().toByteArray()), merge, testDbUuid, testTimestamp, testNodeData)
+        nodes["tailLeafBeforeMerge"] = leaf4
+        return GraphDescription(root, nodes)
     }
 
     @JsName("Test_nodes_between_when_base_after_merge_base")
     @Test
     fun `Test nodes between when base after merge base`(){
         runBlocking {
-            val data = createNodesGraph(BaseGraph.AFTER)
-            val actualList = flow{impl(data.first.first, data.first.second){node -> node as NodeVal<Hash>} }.toList()
-            assertEquals(data.second, actualList)
+            val data = createNodesGraph()
+            val actualList = flow{impl(data.root, data.nodes["tailLeafBeforeMerge"]!!){node -> node as NodeVal<Hash>} }.toList()
+            val expected = listOf(
+                data.nodes["leafBeforeRootAndMergeParent"],
+                data.nodes["leafBeforeRootAfterMergeParent"],
+                data.nodes["mergeParent"],
+                data.nodes["merge"],
+                data.nodes["tailLeafBeforeMerge"],
+            )
+            assertEquals(expected, actualList)
         }
     }
 
@@ -48,9 +56,14 @@ class TrxGraphTest{
     @Test
     fun `Test nodes between when base before merge base`(){
         runBlocking {
-            val data = createNodesGraph(BaseGraph.BEFORE)
-            val actualList = flow{impl(data.first.first, data.first.second){node -> node as NodeVal<Hash>} }.toList()
-            assertEquals(data.second, actualList)
+            val data = createNodesGraph()
+            val actualList = flow{impl(data.nodes["leafBeforeRootAfterMergeParent"], data.nodes["tailLeafBeforeMerge"]!!){node -> node as NodeVal<Hash>} }.toList()
+            val expected = listOf(
+                data.nodes["mergeParent"],
+                data.nodes["merge"],
+                data.nodes["tailLeafBeforeMerge"],
+            )
+            assertEquals(expected, actualList)
         }
     }
 
@@ -58,9 +71,17 @@ class TrxGraphTest{
     @Test
     fun `Test nodes between for all graph`(){
         runBlocking {
-            val data = createNodesGraph(BaseGraph.NONE)
-            val actualList = flow{impl(data.first.first, data.first.second){node -> node as NodeVal<Hash>} }.toList()
-            assertEquals(data.second, actualList)
+            val data = createNodesGraph()
+            val actualList = flow{impl(null, data.nodes["tailLeafBeforeMerge"]!!){node -> node as NodeVal<Hash>} }.toList()
+            val expected = listOf(
+                data.root,
+                data.nodes["leafBeforeRootAndMergeParent"],
+                data.nodes["leafBeforeRootAfterMergeParent"],
+                data.nodes["mergeParent"],
+                data.nodes["merge"],
+                data.nodes["tailLeafBeforeMerge"],
+            )
+            assertEquals(expected, actualList)
         }
     }
 
